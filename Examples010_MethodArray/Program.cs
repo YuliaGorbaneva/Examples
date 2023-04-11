4cc43cc5c6243877ea5b4e06040a38aee61fb507
Yulia <yarantseva89@bk.ru>
2023-04-11 15:30:20 +0600

    method_array

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1,25,3,4,5482,654,8,8,98};
+
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
